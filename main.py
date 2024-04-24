6807432e06bc766dcf4e1b4a912d1b54f9117ce1
mogu <784924036@.com>
2024-04-24 11:31:59 +0800

    update

--- main.py
+++ main.py
@@ -1,33 +1,107 @@
+import sys
+import os
 import cv2
-import numpy as np
+import Ui_wd
+import datetime
 
-def read_img(img_path):
-    return cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+import qt5_applications
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton
+from PyQt5.QtGui import QPalette, QBrush, QPixmap, QImage
+from postpro import Vedio
 
-def main():
-    img_path = 'img.jpg'
-    
-    img = read_img(img_path)
-    
-    _, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    max_contour = max(contours, key=cv2.contourArea)
-    
-    M = cv2.moments(max_contour)
-    
-    center_x = int(M['m10']/M['m00'])
-    center_y = int(M['m01']/M['m00'])
-    
-    print(len(contours))
-    cv2.drawContours(img, contours, -1, (0,0,255), 1)
-    cv2.drawContours(img, [max_contour], 0, (0, 0, 255, 3))
-    cv2.circle(img, (center_x, center_y), 5, (0, 0, 255), -1)
-    
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
+dirname = os.path.dirname(qt5_applications.__file__)
+plugin_path = os.path.join(dirname, 'Qt', 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+
+class MyMainWindow(Ui_wd.Ui_MainWindow, QMainWindow):
+        def __init__(self, parent=None):
+            super(MyMainWindow, self).__init__(parent)
+            self.setupUi(self)
+            self.cap = Vedio()
+            self.timer_camera = QTimer()
+            self.timer_camera.timeout.connect(self.read_cap)
+            self.native_img = None
+            self.postpro_img = None
+            self.pause = False
+
+            
+        def clickStart(self):
+            if self.timer_camera.isActive() == False:
+                success = self.cap.open_cap()
+                if not success:
+                    self.textBrowser.setText('failed to open cap')
+                else:
+                    self.timer_camera.start(30)
+                    self.startButton.setText('Close')
+            else:
+                self.timer_camera.stop()
+                self.startButton.setText('Start')
+            
+        def clickPause(self):
+            self.textBrowser.setText('pause')
+            if self.pause:
+                if not self.timer_camera.isActive():
+                    self.timer_camera.start()
+                    self.pause = False
+                    self.pauseButton.setText('pause')
+            elif not self.pause:
+                if self.timer_camera.isActive():
+                    self.timer_camera.stop()
+                    self.pause = True
+                    self.pauseButton.setText('continue')
+        
+        def clickSave(self):
+            fn = f'{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.jpg'
+            bn = fn
+            cnt = 0
+            while os.path.exists(fn):
+                fn = f'{bn}_{cnt}'
+                cnt += 1
+            if self.postpro_img is not None:
+                cv2.imwrite(fn)
+            
+        def clickExit(self):
+            ok = QPushButton()
+            cancel = QPushButton()
+            msg = QMessageBox(QMessageBox.Warning, 'Exit', 'Confirm exit!!')
+            msg.addButton(ok, QMessageBox.ActionRole)
+            msg.addButton(cancel, QMessageBox.RejectRole)
+            ok.setText('Yes')
+            cancel.setText('No')
+            if msg.exec_() == QMessageBox.RejectRole:
+                return
+            else:
+                self.cap.close_cap()
+                if self.timer_camera.isActive():
+                    self.timer_camera.stop()
+                cv2.destroyAllWindows()
+                self.close()
+        
+        def read_cap(self):
+            from datetime import datetime
+            tt = datetime.now()
+            self.textBrowser.setText(f'{tt}')
+            self.cap.read_cap(self)
+            show_native = QImage(self.native_img.data, 
+                                 self.native_img.shape[1], 
+                                 self.native_img.shape[0],
+                                 QImage.Format_RGB888)
+            self.nativeVideo.setPixmap(QPixmap.fromImage(show_native))
+            show_postpro = QImage(self.postpro_img.data, 
+                                 self.postpro_img.shape[1], 
+                                 self.postpro_img.shape[0],
+                                 QImage.Format_RGB888)
+            self.postproVideo.setPixmap(QPixmap.fromImage(show_postpro))
+        
+        
+def run():
+    app = QApplication(sys.argv)
+    MainWindow = MyMainWindow()
+    MainWindow.show()
     cv2.destroyAllWindows()
+    sys.exit(app.exec_())
     
 if __name__ == '__main__':
-    main()
+    run()
